cc8c77ba1247d8403b3413f15d193a28fa17cdf3
Robert Aleksic <robert.aleksic@gmail.com>
2014-04-16 22:49:57 +0200

    updateall

---
+++ updateall.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+for s in n1deploy amazon-fiat amazon-laki amazon-robby ; do
+  echo "*** server $s ***"
+  ssh $s './upgrdate.sh; sudo reboot' &
+done
+
+echo '*** server arialocal ***'
+ssh arialocal './upgrdate.sh' &
+
+echo '*** localhost ***'
+~/upgrdate.sh
